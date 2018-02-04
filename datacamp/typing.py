dcdd95ca99fe548166806e28901d7d93ce25276e
Otis Anderson <otis@opendoor.com>
2018-02-04 14:17:40 -0800

    notes from types and vars

---
+++ datacamp/typing.py
@@ -0,0 +1,59 @@
+###basics 
+#getting a type
+type()
+
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
+
+# Create a variable savings
+savings = 100
+
+# Create a variable factor
+factor = 1.1
+
+# Calculate result
+result = savings*factor**7. # seven years of interest
+
+# Print out result
+print(result)
+
+# Several variables to experiment with
+savings = 100
+factor = 1.1
+desc = "compound interest"
+
+# Assign product of factor and savings to year1
+year1 = savings*factor
+
+# Print the type of year1
+print(type(year1))
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+
+# Print out doubledesc
+print(doubledesc)
+
+
+###lists 
+#lists are represented in brackets [a,b,c]
+#can have many data types
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# when printed these function sorta like lables.
+areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
+
+# Print areas
+print(areas)
+
+# Print areas
+
